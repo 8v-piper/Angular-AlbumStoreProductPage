6025ef3ef36055b9a52309d119a4e599e81734d1
8v-piper <8v.piper@gmail.com>
2021-12-01 18:31:36 +0200

    Test4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+
+import { Track } from "./track";
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { } //good practice for injecting instances of a serivce into a class
 
-  getAlbum( id: number) {
-    return this._http.get(this._albumUrl).map((response) => response.json())
+  getAlbum( id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map(response => <Album>response.json())
   }
 }
